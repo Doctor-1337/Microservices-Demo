4473b1cad1681602b92d342b5e59e283bd69c890
Mohd Nabeel <doctor@Mohds-MBP.Dlink>
2022-09-09 12:15:34 +0530

    initial commit

---
+++ src/main/java/com/example/Microservicesdepartmentservice/MicroservicesDepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Microservicesdepartmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDepartmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesDepartmentServiceApplication.class, args);
+	}
+
+}
